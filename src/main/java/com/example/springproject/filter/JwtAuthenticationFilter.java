60038c7b7ef2123165eb58629ef122d15a48c4d9
nguyenanhtu123 <lovebooks29062k2@gmail.com>
2024-01-11 14:01:13 +0700

    Update Security

--- src/main/java/com/example/springproject/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/example/springproject/filter/JwtAuthenticationFilter.java
@@ -6,7 +6,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -20,37 +19,37 @@ import java.io.IOException;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-    @Autowired
-    private  JwtService jwtService;
-    @Autowired
-    private  UserDetailsService userDetailsService;
+  @Autowired
+  private JwtService jwtService;
+  @Autowired
+  private UserDetailsService userDetailsService;
 
-    @Override
-    protected void doFilterInternal(
-            @NonNull HttpServletRequest request,
-            @NonNull HttpServletResponse response,
-            @NonNull FilterChain filterChain) throws ServletException, IOException {
-        final String authHeader = request.getHeader("Authorization");
-        final String jwt;
-        final String userName;
-        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-            filterChain.doFilter(request, response);
-            return;
-        }
-        jwt = authHeader.substring(7);
-        userName = jwtService.extractUsername(jwt);
-        if (userName != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userName);
-            if (jwtService.isTokenValid(jwt, userDetails)) {
-                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
-                        userDetails,
-                        null,
-                        userDetails.getAuthorities()
-                );
-                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-            }
-        }
-        filterChain.doFilter(request, response);
+  @Override
+  protected void doFilterInternal(
+        @NonNull HttpServletRequest request,
+        @NonNull HttpServletResponse response,
+        @NonNull FilterChain filterChain) throws ServletException, IOException {
+    final String authHeader = request.getHeader("Authorization");
+    final String jwt;
+    final String userName;
+    if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+      filterChain.doFilter(request, response);
+      return;
     }
+    jwt = authHeader.substring(7);
+    userName = jwtService.extractUsername(jwt);
+    if (userName != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+      UserDetails userDetails = this.userDetailsService.loadUserByUsername(userName);
+      if (jwtService.isTokenValid(jwt, userDetails)) {
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+              userDetails,
+              null,
+              userDetails.getAuthorities()
+        );
+        authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+      }
+    }
+    filterChain.doFilter(request, response);
+  }
 }
--- src/main/java/com/example/springproject/service/impl/AuthenticationService.java
+++ src/main/java/com/example/springproject/service/impl/AuthenticationService.java
@@ -18,34 +18,34 @@ import java.util.Date;
 
 @Service
 public class AuthenticationService {
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+  @Autowired
+  private PasswordEncoder passwordEncoder;
 
-    @Autowired
-    private UserRepository userRepository;
+  @Autowired
+  private UserRepository userRepository;
 
-    @Autowired
-    private JwtService jwtService;
+  @Autowired
+  private JwtService jwtService;
 
-    @Autowired
-    private AuthenticationManager authenticationManager;
+  @Autowired
+  private AuthenticationManager authenticationManager;
 
 
-    public ResponseGeneral<AuthenticationResponse> register(AuthenticationRequest dto){
-        User user = MapperUtils.toEntity(dto, User.class);
-        user.setPassword(passwordEncoder.encode(dto.getPassword()));
-        user.setCreatedAt(DateUtils.getCurrentTimeMillis());
-        userRepository.save(user);
-        return ResponseGeneral.ofCreated("Success!", AuthenticationResponse.builder()
-                        .token(jwtService.generateToken(new CustomUserDetail(user)))
-                .build());
-    }
+  public ResponseGeneral<AuthenticationResponse> register(AuthenticationRequest dto) {
+    User user = MapperUtils.toEntity(dto, User.class);
+    user.setPassword(passwordEncoder.encode(dto.getPassword()));
+    user.setCreatedAt(DateUtils.getCurrentTimeMillis());
+    userRepository.save(user);
+    return ResponseGeneral.ofCreated("Success!", AuthenticationResponse.builder()
+          .token(jwtService.generateToken(new CustomUserDetail(user)))
+          .build());
+  }
 
-    public ResponseGeneral<AuthenticationResponse> authenticate(AuthenticationRequest authenticationRequest){
-        var token = new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword());
-        CustomUserDetail customUserDetail = (CustomUserDetail) authenticationManager.authenticate(token).getPrincipal();
-        return ResponseGeneral.ofSuccess("User has been authenticated!", AuthenticationResponse.builder()
-                        .token(jwtService.generateToken(customUserDetail))
-                .build());
-    }
+  public ResponseGeneral<AuthenticationResponse> authenticate(AuthenticationRequest authenticationRequest) {
+    var token = new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword());
+    CustomUserDetail customUserDetail = (CustomUserDetail) authenticationManager.authenticate(token).getPrincipal();
+    return ResponseGeneral.ofSuccess("User has been authenticated!", AuthenticationResponse.builder()
+          .token(jwtService.generateToken(customUserDetail))
+          .build());
+  }
 }
